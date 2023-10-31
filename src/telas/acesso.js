066334cea07bd261fb85c4b60ea7b4d1e968c13c
Tarcisio Pinheiro <tarcioswin@hotmail.com>
2023-10-31 16:12:00 -0300

    Tela de acesso modificada

--- src/telas/acesso.js
+++ src/telas/acesso.js
@@ -4,11 +4,16 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import { Ionicons } from '@expo/vector-icons';
 import COLORS from '../../components/colors';
 import Boi from '../../assets/logoboi.png';
+import axios from 'axios';
+import { LineChart } from 'react-native-svg-charts';
+import * as shape from 'd3-shape';
 
 const Acesso = ({ navigation }) => {
   const [backgroundColor, setBackgroundColor] = useState(COLORS.white);
   const [boiHeaderImageWidth, setBoiHeaderImageWidth] = useState(30);
   const [boiContentImageWidth, setBoiContentImageWidth] = useState(100);
+  const [dollarPrice, setDollarPrice] = useState('');
+  const [chartData, setChartData] = useState([]);
 
   useLayoutEffect(() => {
     const windowWidth = Dimensions.get('window').width;
@@ -46,11 +51,50 @@ const Acesso = ({ navigation }) => {
     });
   }, [backgroundColor]);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo';
+        const response = await axios.get(url);
+        const timeSeries = response.data['Time Series (Daily)'];
+        if (timeSeries) {
+          const chartDataArray = [];
+          Object.keys(timeSeries).forEach((date) => {
+            const dataPoint = timeSeries[date];
+            const closeValue = parseFloat(dataPoint['5. volume']);
+            chartDataArray.push(closeValue);
+          });
+          setChartData(chartDataArray);
+
+          const latestData = Object.values(timeSeries)[0];
+          const latestDollarPrice = parseFloat(latestData['4. close']);
+          setDollarPrice(latestDollarPrice);
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.white }}>
       <View style={{ flex: 1 }}>
         <Image source={Boi} style={[styles.boiImage, { width: boiContentImageWidth, height: boiContentImageWidth }]} />
         <Text style={styles.titleText}>Cotação do Boi</Text>
+        <Text>(IBM) close stock quote: {dollarPrice || 'Loading...'}</Text>
+
+        {/* Line chart */}
+        <LineChart
+          style={{ flex: 1 }}
+          data={chartData}
+          contentInset={{ top: 20, bottom: 20 }}
+          svg={{ stroke: 'rgb(255, 0, 0)' }}
+          curve={shape.curveBasis}
+          yMin={0}
+          yMax={Math.max(...chartData) + 10}
+        />
       </View>
     </SafeAreaView>
   );
@@ -84,9 +128,9 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     paddingVertical: 6,
     paddingHorizontal: 12,
-    alignSelf: 'flex-end', // Align to the right
-    marginTop: 'auto', // Vertically center
-    marginBottom: 16, // Add some margin at the bottom
+    alignSelf: 'flex-end',
+    marginTop: 'auto',
+    marginBottom: 16,
   },
   editButtonText: {
     color: COLORS.white,
