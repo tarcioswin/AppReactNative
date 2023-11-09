053c4f0d13859557eedbaafd1361f0b358b98114
Tarcisio Pinheiro <tarcioswin@hotmail.com>
2023-11-09 11:09:31 -0300

    Gafico_V7

--- src/telas/acesso.js
+++ src/telas/acesso.js
@@ -1,23 +1,19 @@
-import React, { useState, useEffect, useLayoutEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, Button } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Ionicons } from '@expo/vector-icons';
 import COLORS from '../../components/colors';
 import axios from 'axios';
-import { Grid, YAxis, XAxis, LineChart } from 'react-native-svg-charts';
-import * as shape from 'd3-shape';
-import * as scale from 'd3-scale';
-import ModalDropdown from 'react-native-modal-dropdown';
 import moment from 'moment';
-
-
+import { Picker } from '@react-native-picker/picker';
+import { VictoryLabel, VictoryChart, VictoryLine, VictoryArea, VictoryAxis, VictoryVoronoiContainer, VictoryTooltip, VictoryCursorContainer } from 'victory-native';
 
 
 const Acesso = ({ navigation }) => {
   const [backgroundColor, setBackgroundColor] = useState(COLORS.white);
   const [Price, setPrice] = useState('');
   const [chartData, setChartData] = useState([]);
-  const [selectedInfo, setSelectedInfo] = useState('close'); // Default selected option
+  const [selectedInfo, setSelectedInfo] = useState('close');
   const [infoOptions] = useState(['1. open', '2. high', '3. low', '4. close', '5. volume']);
   const [showGraph, setShowGraph] = useState(false);
 
@@ -29,50 +25,52 @@ const Acesso = ({ navigation }) => {
       headerTitleStyle: { fontWeight: 'bold' },
       headerRight: () => (
         <TouchableOpacity
-          onPress={() => {
-            navigation.navigate('EditInformation');
-          }}
+          onPress={() => navigation.navigate('EditInformation')}
           style={styles.editButton}
         >
-          <Ionicons
-            name="pencil-outline"
-            size={20}
-            color={COLORS.white}
-            style={{ marginRight: 8 }}
-          />
-          <Text style={styles.editButtonText}>Edit Inf. </Text>
+          <Ionicons name="pencil-outline" size={20} color={COLORS.white} />
+          <Text style={styles.editButtonText}>Edit Inf.</Text>
         </TouchableOpacity>
       ),
     });
-  }, [backgroundColor]);
+  }, [backgroundColor, navigation]);
 
 
 
 
 
   const fetchData = async () => {
+    const apiKey = 'demo'; // Use your actual API key here
+    const symbol = 'IBM';
+    const functionType = 'TIME_SERIES_DAILY';
+    const url = `https://www.alphavantage.co/query?function=${functionType}&symbol=${symbol}&apikey=${apiKey}`;
+
     try {
-      const url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo';
       const response = await axios.get(url);
       const timeSeries = response.data['Time Series (Daily)'];
 
       if (timeSeries) {
         const twoWeeksAgo = new Date();
-        twoWeeksAgo.setDate(twoWeeksAgo.getDate() - 14); // Set the date to 2 weeks in the past
+        twoWeeksAgo.setDate(twoWeeksAgo.getDate() - 15);
 
-        const chartDataArray = [];
-        Object.entries(timeSeries)
-          .filter(([date, _]) => new Date(date) >= twoWeeksAgo) // Filter out dates that are more than 2 weeks old
-          .forEach(([date, dataPoint]) => {
-            const selectedValue = parseFloat(dataPoint[selectedInfo]);
-            chartDataArray.push({ date, value: selectedValue });
-          });
-        setChartData(chartDataArray.reverse()); // The API returns dates in descending order, so we reverse it for the chart
+        const infoKeySuffix = selectedInfo.split('. ')[1]; // Get the suffix after the dot and space (e.g., 'open' from '1. open')
 
-        const latestData = Object.values(timeSeries)[0];
-        const latestDollarPrice = parseFloat(latestData[selectedInfo]);
-        setPrice(latestDollarPrice);
-        setShowGraph(true); // Show the graph after fetching data
+        // Make sure the suffix is a string before calling toLowerCase
+        const infoKey = typeof infoKeySuffix === 'string' ? infoKeySuffix.toLowerCase() : '';
+
+        const processedChartData = Object.entries(timeSeries)
+          .filter(([date, _]) => new Date(date) >= twoWeeksAgo)
+          .map(([date, dataPoint]) => {
+            // Find the correct data point key that ends with the selectedInfo suffix (e.g., '4. close' when infoKey is 'close')
+            const dataPointKey = Object.keys(dataPoint).find(key => key.endsWith(infoKey));
+            const selectedValue = dataPointKey ? parseFloat(dataPoint[dataPointKey]) : null;
+            return { x: new Date(date), y: selectedValue };
+          })
+          .reverse(); // Reverse to have the earliest date first
+
+        setChartData(processedChartData);
+        setPrice(processedChartData.length ? processedChartData[0].y : '');
+        setShowGraph(true);
       }
     } catch (error) {
       console.error('Error fetching data:', error);
@@ -80,110 +78,128 @@ const Acesso = ({ navigation }) => {
   };
 
 
+  // Function to handle the cursor movement and set state
+  const handleCursorChange = (value, props) => {
+    const chartData = props.scale.y.domain();
+    const closestPoint = chartData.reduce((prev, curr) =>
+      Math.abs(curr.x - value.x) < Math.abs(prev.x - value.x) ? curr : prev
+    );
+    setCursorValue(closestPoint);
+  };
 
-  const yValues = chartData.map((item) => item.value);
-  const yMin = Math.min(...yValues);
-  const yMax = Math.max(...yValues);
 
 
   return (
-
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.white }}>
       <View style={{ flex: 1 }}>
         <Text style={styles.centeredText}>
-          (IBM) {selectedInfo.split('. ')[1]} stock quote last day: {Price}
+          (IBM) {selectedInfo.split(' ')[1]} stock quote last day: {Price}
         </Text>
 
 
-
-
-        <View style={styles.dropdownContainer}>
-          <ModalDropdown
-            options={['Select an item', ...infoOptions]}
-            defaultValue="Select an item"
-            textStyle={styles.dropdownText}
-            dropdownTextStyle={styles.dropdownItemText}
-            initialScrollIndex={1} // Ensure this index is within the range of your list items
-            onSelect={(index, value) => {
-              if (index === 0) {
-                setSelectedInfo(null);
-              } else {
-                setSelectedInfo(value);
-              }
-            }}
-          />
+        <View style={styles.pickerContainer}>
+          <Picker
+            selectedValue={selectedInfo}
+            onValueChange={(value) => setSelectedInfo(value)}
+            style={styles.dropdownPicker}
+            itemStyle={{ color: 'black', fontSize: 16 }}
+          >
+            <Picker.Item label="Select an item" value="" />
+            {infoOptions.map((option, index) => (
+              <Picker.Item key={index} label={option} value={option} />
+            ))}
+          </Picker>
         </View>
 
 
         <View
           style={{
             marginTop: 10, marginBottom: 4, backgroundColor: '#35AAFF', fontWeight: 'bold', width: '60%', alignSelf: 'center',
-            borderRadius: 80,
+            borderRadius: 8,
           }}
-        ><Button title="Show Graph" onPress={fetchData} />
+        >
+          <Button
+            title="Show Graph"
+            onPress={() => {
+              setShowGraph(false); // Hide the graph
+              fetchData(selectedInfo); // Fetch new data
+            }}
+          />
         </View>
 
 
 
-
         {showGraph && (
-          <View style={{ flexDirection: 'row', height: 500, paddingVertical: 0 }}>
-            <YAxis
-              data={chartData}
-              yAccessor={({ item }) => item.value}
-              contentInset={{ top: 30, bottom: 100 }}
-              svg={{
-                fill: 'black',
-                fontSize: 12,
-                fontWeight: 'bold', // Bold font weight for X axis labels
-              }}
-              numberOfTicks={8}
-              formatLabel={(value) => `${value}`}
-              style={{ marginBottom: 10, marginLeft: 10 }}
-            />
-            <View style={{ flex: 1, marginLeft: 10, marginRight: 10 }}>
-              <LineChart
-                style={{ flex: 1 }}
-                data={chartData}
-                curve={shape.curveNatural}
-                svg={{
-                  stroke: 'rgb(255, 0, 0)', // Line color (red)
-                  strokeWidth: 2, // Line width
-                  strokeOpacity: 0.7, // Line opacity (0 to 1)
-                  fill: 'rgba(255, 0, 0, 0.2)', // Area under the line color
-                  fillOpacity: 1, // Area under the line opacity (0 to 1)
-                }}
-                showGrid={true}
-                contentInset={{ top: 30, bottom: 30 }}
-                yAccessor={({ item }) => item.value}
-                xScale={scale.scaleTime}
-                xAccessor={({ item }) => new Date(item.date)}
-                yMin={yMin}
-                yMax={yMax}
-              >
-                <Grid />
-              </LineChart>
-              <XAxis
-                style={{ height: 80, marginHorizontal: -100 }}
-                data={chartData}
-                xAccessor={({ item }) => new Date(item.date)}
-                formatLabel={value => moment(value).format('DD MMM')}
-                contentInset={{ left: 100, right: 110 }}
-                svg={{
-                  fontSize: 14,
-                  fill: 'black',
-                  fontWeight: 'bold',
-                  rotation: 90,
-                  originY: 35,
-                  y: 1,
-                }}
-                scale={scale.scaleTime}
-                numberOfTicks={10}
+          <VictoryChart
+            domainPadding={{ y: 60 }}
+            height={400} // Height of the chart
+            width={440}  // Width of the chart
+            containerComponent={
+              <VictoryCursorContainer
+                cursorLabel={({ datum }) => `${datum.y.toFixed(0)}\n${moment(datum.x).format('MMM D')}`}
+                cursorLabelComponent={<VictoryLabel dy={-60} style={{ fontSize: 14, fontWeight: 'bold' }} />} // Moves the label up by 20 units
               />
-
-            </View>
-          </View>
+              // or tooltip
+              // <VictoryVoronoiC ontainer
+              //   voronoiDimension="x"
+              //   labelComponent={<VictoryTooltip flyoutStyle={{ fill: "white" }} />}
+              // />
+            }
+          >
+            <VictoryLine
+              animate={{
+                duration: 1200,
+                onLoad: { duration: 1200 }
+              }}
+              data={chartData}
+              style={{
+                data: {
+                  fill: "rgba(135, 206, 235, 0.4)", // Light blue fill with some transparency
+                  stroke: "#1E90FF", // Darker blue stroke
+                  strokeWidth: 3, // Thicker stroke width
+                }
+              }}
+            />
+            <VictoryAxis // X axis
+              //label="Date"
+              tickCount={5}
+              tickFormat={(t) => moment(t).format('MMM D')}
+              style={{
+                grid: { stroke: "black", strokeWidth: 0.5 },
+                ticks: { stroke: "black", size: 5 },
+                tickLabels: { 
+                  fontSize: 13,
+                  padding: 5,
+                  fontWeight: 'bold',
+                  angle: 0, // Rotate labels by 90 degrees
+                  //verticalAnchor: 'middle', // Adjusts the label vertically
+                  //textAnchor: 'start' // Adjusts the label horizontally
+                }
+              }}
+            />
+            <VictoryAxis // Y axis
+              dependentAxis
+              tickCount={10}
+              tickFormat={(tick) => {
+                if (tick >= 1e6) {
+                  return `${tick / 1e6}M`; // Convert to "M" for millions
+                } else if (tick >= 1e3) {
+                  return `${tick / 1e3}k`; // Convert to "k" for thousands
+                }
+                return tick; // For numbers less than 1000, return the original tick
+              }}
+              style={{
+                grid: { stroke: "black", strokeWidth: 0.5 },
+                ticks: { stroke: "black", size: 8 },
+                tickLabels: { fontSize: 13, padding: 5, fontWeight: 'bold' }
+              }}
+            />
+          </VictoryChart>
         )}
+
+
+
+
 
 
       </View>
@@ -194,30 +210,38 @@ const Acesso = ({ navigation }) => {
 
 
 
-
 const styles = StyleSheet.create({
+  axisStyle: {
+    axis: { stroke: 'grey' },
+    axisLabel: { fontSize: 16, padding: 30 },
+    grid: { stroke: 'grey', strokeWidth: 0.5 },
+    ticks: { stroke: 'grey', size: 5 },
+    tickLabels: { fontSize: 10, padding: 5 },
+  },
+  pickerContainer: {
+    borderWidth: 2,
+    borderColor: 'black',
+    borderRadius: 8,
+    overflow: 'hidden',
+    alignItems: 'center',  // Center horizontally
+    justifyContent: 'center',  // Center vertically
+    height: 40, // Set a fixed height for the dropdown
+    width: 200,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
   centeredText: {
-    textAlign: 'center', // Horizontally center the text
-    alignSelf: 'center', // Center the text within its container
+    textAlign: 'center',
+    alignSelf: 'center',
     fontSize: 16,
     fontWeight: 'bold',
-    // Add any other styles you need
   },
-  dropdownView: {
-    width: 130, // Adjust the width as needed
-    borderWidth: 1,
-    borderColor: 'gray',
-    borderRadius: 8,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  dropdownText: {
-    fontSize: 16,
-    padding: 10,
-    borderWidth: 1,
-    borderColor: 'gray',
-    borderRadius: 8,
-    backgroundColor: '#fafafa',
+  tooltipText: {
+    textAlign: 'center',
+    alignSelf: 'center',
+    fontSize: 14,
+    fontWeight: 'bold',
+    marginTop: 5,
   },
   dropdownContainer: {
     width: 150,
@@ -228,34 +252,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  dropdownItemText: {
-    fontSize: 16,
-    padding: 10,
-  },
-  headerTitleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  boiImage: {
-    borderRadius: 15,
-    alignSelf: 'center',
-    marginVertical: 0,
-  },
-  headerTitle: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    left: 10,
-  },
-  titleText: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginVertical: 0,
-    alignSelf: 'center',
+  dropdownPicker: {
+    width: 150,
+    height: 40,
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 8,
+    backgroundColor: '#fafafa',
   },
   editButton: {
     flexDirection: 'row',
     alignItems: 'center',
-    backgroundColor: COLORS.primary,
+    backgroundColor: '#35AAFF',
     borderRadius: 8,
     paddingVertical: 6,
     paddingHorizontal: 12,
@@ -271,7 +279,3 @@ const styles = StyleSheet.create({
 });
 
 export default Acesso;
-
-
-
-
